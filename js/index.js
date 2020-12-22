71e211693328a6122bea2e7d2f51b746daf15fc0
Serhii <Sergeyuhimenko@gmail.com>
2020-12-22 18:08:33 +0000

    Js counter

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const startTime = new Date();
+function updateTimer() {
+    const currTime = new Date();
+    const diffSecs = Math.round((currTime - startTime) / 1000);
+    document.querySelector('.banner-counter').innerText = diffSecs * 42;
+}
+setInterval(updateTimer, 1000);
