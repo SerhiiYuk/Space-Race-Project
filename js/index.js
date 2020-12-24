6a3bf7dff629a54c0f039a8c4f914819541bf0b5
Serhii <Sergeyuhimenko@gmail.com>
2020-12-24 17:38:28 +0000

    one more section

--- js/index.js
+++ js/index.js
@@ -5,9 +5,3 @@ function updateTimer() {
     document.querySelector('.banner-counter').innerText = diffSecs * 42;
 }
 setInterval(updateTimer, 1000);
-
-function updateClock() {
-    const clock = document.querySelector('.clock');
-    clock.innerText = new Date().toLocaleTimeString();
-}
-setInterval(updateClock, 1000);
